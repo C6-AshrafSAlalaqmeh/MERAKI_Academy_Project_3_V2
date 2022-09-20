7d1f096c7da6d4447c5d033726fe180e116eb126
AshrafSAlalaqmeh <Ashraf.Alalaqmeh@gmail.com>
2022-09-20 18:20:49 +0300

    Edit style Dashboard

--- frontend/src/components/Dashboard/index.js
+++ frontend/src/components/Dashboard/index.js
@@ -1,44 +1,60 @@
+import "./style.css"
 import axios from "axios"
 import { useEffect, useState } from "react"
 
 
 
 const Dashboard =({token})=>{
-    const [article , setArticle]=useState([])
-  
-  
-  useEffect(()=>{
+    const [article , setArticle]=useState([])  
 
+
+    useEffect(()=>{
 axios.get('http://localhost:5000/articles',{
-    headers : 'Bearer' + token
+    headers :{
+        authorization: 'Bearer ' + token
+    }
 })
 .then((result)=>{
-console.log(result)
-setArticle(result)
+console.log(result.data.articles)
+setArticle(result.data.articles)
 })
 .catch((err)=>{
-    console.log(err)
+    console.log(err.message)
 })
 
-  })
+  },[])
   
   
   
     return (
     
     <>
-    <h1> Dashboard</h1>
-    {article.map((elem)=>{
-        return (
-            <>
-        <p>{elem.title}</p>
-        <p>{elem.description}</p>
-        <button>Delete</button>
-        <button>update</button>
-        <button> Adding Comment</button>
-        </>
+   
+    
+     {article.map((elem)=>{
+    
+      return (
+            <div className="bodydash">
+            <div className="contanrDash">
+         <div className="titileDesc">       
+        <p className="titleDash">{elem.title}</p>
+        <p className="descDash">{elem.description}</p>
+        </div>
+        <div className="buttonAndInputDash">
+        <div className="inputIndash">
+         <input className="inputIndiv" placeholder="Comment..." />
+         </div>
+         <div className="allbutton">
+        <button className="add"> Adding Comment</button>
+        <button className="update">update</button>
+        <button className="delete"> Delete </button>
+        </div>
+        </div>
+        </div>
+        
+        </div>
         )
-    })}
+    })} 
     
     
     </>
